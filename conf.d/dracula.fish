150bf157b5496526b900d53dd8bd3f674a62f670
zhaolei <im.zhaolei@foxmail.com>
2021-03-16 11:33:53 +0800

    refactor dracula theme

--- conf.d/dracula.fish
+++ conf.d/dracula.fish
@@ -1,35 +1,38 @@
+#!/usr/bin/env fish
+# fish internal theme dracula
+
 if not set -q __set_fish_theme
     set -U __set_fish_theme
 
-    # fish internal theme dracula
-    set -U fish_color_normal normal
-    set -U fish_color_command F8F8F2
-    set -U fish_color_quote F1FA8C
-    set -U fish_color_redirection 8BE9FD
-    set -U fish_color_end 50FA7B
-    set -U fish_color_error FFB86C
-    set -U fish_color_param FF79C6
-    set -U fish_color_comment 6272A4
-    set -U fish_color_match --background=brblue
-    set -U fish_color_selection white --bold --background=brblack
-    set -U fish_color_search_match bryellow --background=brblack
-    set -U fish_color_history_current --bold
-    set -U fish_color_operator 00a6b2
-    set -U fish_color_escape 00a6b2
-    set -U fish_color_cwd green
-    set -U fish_color_cwd_root red
-    set -U fish_color_valid_path --underline
-    set -U fish_color_autosuggestion BD93F9
-    set -U fish_color_user brgreen
-    set -U fish_color_host normal
-    set -U fish_color_cancel -r
-    set -U fish_pager_color_completion normal
-    set -U fish_pager_color_description B3A06D yellow
-    set -U fish_pager_color_prefix white --bold --underline
-    set -U fish_pager_color_progress brwhite --background=cyan
+    # Syntax Highlighting Colors
+    set -U fish_color_autosuggestion    BD93F9
+    set -U fish_color_cancel            -r
+    set -U fish_color_command           F8F8F2
+    set -U fish_color_comment           6272A4
+    set -U fish_color_cwd               green
+    set -U fish_color_cwd_root          red
+    set -U fish_color_end               50FA7B
+    set -U fish_color_error             FFB86C
+    set -U fish_color_escape            00A6B2
+    set -U fish_color_history_current   --bold
+    set -U fish_color_host              normal
+    set -U fish_color_host_remote       yellow
+    set -U fish_color_keyword           BD93F9
+    set -U fish_color_match             --background=brblue
+    set -U fish_color_normal            normal
+    set -U fish_color_operator          00A6B2
+    set -U fish_color_param             FF79C6
+    set -U fish_color_quote             F1FA8C
+    set -U fish_color_redirection       8BE9FD
+    set -U fish_color_search_match      bryellow --background=brblack
+    set -U fish_color_selection         white --bold --background=brblack
+    set -U fish_color_status            red
+    set -U fish_color_user              brgreen
+    set -U fish_color_valid_path        --underline
 
-    # 新增或调整默认dracula主题样式
-    set -U fish_color_command green
-    set -U fish_color_error FF5555
-    set -U fish_color_keyword FF79C6
+    # Completion Pager Colors
+    set -U fish_pager_color_completion  normal
+    set -U fish_pager_color_description B3A06D yellow
+    set -U fish_pager_color_prefix      FF79C6
+    set -U fish_pager_color_progress    brwhite --background=cyan
 end
